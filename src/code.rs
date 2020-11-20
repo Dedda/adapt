879eac758fe837c7ce7c7d95cb90cc3055c84cbd
Stephan Göppentin <dedda102@gmail.com>
2020-11-20 17:04:00 +0100

    some cleaning up

--- src/code.rs
+++ src/code.rs
@@ -18,6 +18,8 @@ pub enum Code {
     Copy(usize, usize),
     Add(usize, usize),
     Sub(usize, usize),
+    Mul(usize, usize),
+    Div(usize, usize),
     Print(usize),
     Exit(i32),
 }
@@ -27,20 +29,17 @@ impl Code {
         match self {
             Self::FlipType(address_at) => {
                 let address = read_int_from_address(&code, address_at.clone())?;
-                if let Some(data) = code.get(address.clone()) {
-                    let address = address.clone();
-                    let data = data.clone();
-                    if let Code::IntData(i) = data {
-                        let _ = std::mem::replace(&mut code[address], Code::CharData(char::from(i.clone() as u8)));
-                        Ok(vec![Actions::Sync])
-                    } else if let Code::CharData(c) = data {
-                        let _ = std::mem::replace(&mut code[address], Code::IntData(c.clone() as usize));
-                        Ok(vec![Actions::Sync])
-                    } else {
-                        Err(format!("Value at {} is not char or int", address))
-                    }
+                let data = read_from_address(&code, address.clone())?;
+                let address = address.clone();
+                let data = data.clone();
+                if let Code::IntData(i) = data {
+                    let _ = std::mem::replace(&mut code[address], Code::CharData(char::from(i.clone() as u8)));
+                    Ok(vec![Actions::Sync])
+                } else if let Code::CharData(c) = data {
+                    let _ = std::mem::replace(&mut code[address], Code::IntData(c.clone() as usize));
+                    Ok(vec![Actions::Sync])
                 } else {
-                    Err(format!("Cannot load address {}", address))
+                    Err(format!("Value at {} is not char or int", address))
                 }
             },
             Self::Jump(addr ) => Ok(vec![Actions::Jump(addr.clone())]),
@@ -51,48 +50,54 @@ impl Code {
             Self::Swap(addr_addr_1, addr_addr_2) => {
                 let addr_1 = read_int_from_address(&code, addr_addr_1.clone())?;
                 let addr_2 = read_int_from_address(&code, addr_addr_2.clone())?;
-                if let (Some(data_1), Some(data_2)) = (code.get(addr_1.clone()), code.get(addr_2.clone())) {
-                    let data_1 = data_1.clone();
-                    let data_2 = data_2.clone();
-                    let _ = std::mem::replace(&mut code[addr_1], data_2);
-                    let _ = std::mem::replace(&mut code[addr_2], data_1);
-                    Ok(vec![Actions::Sync])
-                } else {
-                    Err(format!("{} or {} are not valid addresses", addr_1, addr_2))
-                }
+                let data_1 = read_from_address(&code, addr_1)?.clone();
+                let data_2 = read_from_address(&code, addr_2)?.clone();
+                let _ = std::mem::replace(&mut code[addr_1], data_2);
+                let _ = std::mem::replace(&mut code[addr_2], data_1);
+                Ok(vec![Actions::Sync])
             },
             Self::Copy(addr_addr_1, addr_addr_2) => {
                 let addr_1 = read_int_from_address(&code, addr_addr_1.clone())?;
                 let addr_2 = read_int_from_address(&code, addr_addr_2.clone())?;
-                if let Some(data_1) = code.get(addr_1.clone()) {
-                    let data_1 = data_1.clone();
-                    let _ = std::mem::replace(&mut code[addr_2], data_1);
-                    Ok(vec![Actions::Sync])
-                } else {
-                    Err(format!("{} is not a valid address", addr_1))
-                }
+                let data_1 = read_from_address(&code, addr_1)?.clone();
+                let _ = std::mem::replace(&mut code[addr_2], data_1);
+                Ok(vec![Actions::Sync])
             },
             Self::Add(addr_addr_1, addr_addr_2) => {
                 let addr_1 = read_int_from_address(&code, addr_addr_1.clone())?;
                 let addr_2 = read_int_from_address(&code, addr_addr_2.clone())?;
-                if let (Some(Code::IntData(int_1)), Some(Code::IntData(int_2))) = (code.get(addr_1.clone()), code.get(addr_2.clone())) {
-                    let added = Code::IntData(int_1 + int_2);
-                    let _ = std::mem::replace(&mut code[addr_2], added);
-                    Ok(vec![Actions::Sync])
-                } else {
-                    Err(format!("{} or {} are not valid int addresses", addr_1, addr_2))
-                }
+                let int_1 = read_int_from_address(&code, addr_1)?;
+                let int_2 = read_int_from_address(&code, addr_2)?;
+                let added = Code::IntData(int_1 + int_2);
+                let _ = std::mem::replace(&mut code[addr_2], added);
+                Ok(vec![Actions::Sync])
             },
             Self::Sub(addr_addr_1, addr_addr_2) => {
                 let addr_1 = read_int_from_address(&code, addr_addr_1.clone())?;
                 let addr_2 = read_int_from_address(&code, addr_addr_2.clone())?;
-                if let (Some(Code::IntData(int_1)), Some(Code::IntData(int_2))) = (code.get(addr_1.clone()), code.get(addr_2.clone())) {
-                    let subbed = Code::IntData(int_1 - int_2);
-                    let _ = std::mem::replace(&mut code[addr_2], subbed);
-                    Ok(vec![Actions::Sync])
-                } else {
-                    Err(format!("{} or {} are not valid int addresses", addr_1, addr_2))
-                }
+                let int_1 = read_int_from_address(&code, addr_1)?;
+                let int_2 = read_int_from_address(&code, addr_2)?;
+                let subbed = Code::IntData(int_1 - int_2);
+                let _ = std::mem::replace(&mut code[addr_2], subbed);
+                Ok(vec![Actions::Sync])
+            },
+            Self::Mul(addr_addr_1, addr_addr_2) => {
+                let addr_1 = read_int_from_address(&code, addr_addr_1.clone())?;
+                let addr_2 = read_int_from_address(&code, addr_addr_2.clone())?;
+                let int_1 = read_int_from_address(&code, addr_1)?;
+                let int_2 = read_int_from_address(&code, addr_2)?;
+                let mul = Code::IntData(int_1 * int_2);
+                let _ = std::mem::replace(&mut code[addr_2], mul);
+                Ok(vec![Actions::Sync])
+            },
+            Self::Div(addr_addr_1, addr_addr_2) => {
+                let addr_1 = read_int_from_address(&code, addr_addr_1.clone())?;
+                let addr_2 = read_int_from_address(&code, addr_addr_2.clone())?;
+                let int_1 = read_int_from_address(&code, addr_1)?;
+                let int_2 = read_int_from_address(&code, addr_2)?;
+                let div = Code::IntData(int_1 / int_2);
+                let _ = std::mem::replace(&mut code[addr_2], div);
+                Ok(vec![Actions::Sync])
             },
             Self::Print(addr_addr) => {
                 let addr = read_int_from_address(&code, addr_addr.clone())?;
@@ -127,20 +132,27 @@ impl Code {
             Copy(addr_addr_1, addr_addr_2) => format!("copy {} {}", addr_addr_1, addr_addr_2),
             Add(addr_addr_1, addr_addr_2) => format!("add {} {}", addr_addr_1, addr_addr_2),
             Sub(addr_addr_1, addr_addr_2) => format!("sub {} {}", addr_addr_1, addr_addr_2),
+            Mul(addr_addr_1, addr_addr_2) => format!("mul {} {}", addr_addr_1, addr_addr_2),
+            Div(addr_addr_1, addr_addr_2) => format!("div {} {}", addr_addr_1, addr_addr_2),
             Print(addr_addr) => format!("print {}", addr_addr),
             Exit(code) => format!("exit {}", code),
         }
     }
 }
 
-fn read_int_from_address(code: &Vec<Code>, int_at: usize) -> std::result::Result<usize, String> {
-    if let Some(int_cell) = code.get(int_at) {
-        if let Code::IntData(int) = int_cell {
-            Ok(int.clone())
-        } else {
-            Err(format!("Data at {} is not an integer", int_at))
-        }
+fn read_from_address(code: &Vec<Code>, addr: usize) -> std::result::Result<&Code, String> {
+    if let Some(cell) = code.get(addr) {
+        Ok(cell)
     } else {
-        Err(format!("Cannot load address {}", int_at))
+        Err(format!("Cannot load address {}", addr))
+    }
+}
+
+
+fn read_int_from_address(code: &Vec<Code>, int_at: usize) -> std::result::Result<usize, String> {
+    if let Code::IntData(int) = read_from_address(code, int_at)? {
+        Ok(int.clone())
+    } else {
+        Err(format!("Data at {} is not an integer", int_at))
     }
 }
--- src/parse.rs
+++ src/parse.rs
@@ -12,43 +12,49 @@ pub fn parse(data: &str) -> Result<Vec<Code>, String> {
 
 fn parse_line(line: &str) -> Result<Code, String> {
     if Regex::new(r"flip type \d+").unwrap().is_match(line) {
-        Ok(FlipType(line.split(' ').last().unwrap().parse().unwrap()))
+        Ok(FlipType(line[10..].parse().unwrap()))
     } else if Regex::new(r"jump \d+").unwrap().is_match(line) {
-        Ok(Jump(line.split(' ').last().unwrap().parse().unwrap()))
+        Ok(Jump(line[5..].parse().unwrap()))
     } else if Regex::new(r"jump addr \d+").unwrap().is_match(line) {
-        Ok(JumpAddr(line.split(' ').last().unwrap().parse().unwrap()))
+        Ok(JumpAddr(line[10..].parse().unwrap()))
     } else if Regex::new(r"swap \d+ \d+").unwrap().is_match(line) {
-        let mut split = line.split(' ');
-        split.next();
+        let mut split = line[5..].split(' ');
         let first = split.next().unwrap();
         let second = split.next().unwrap();
         Ok(Swap(first.parse().unwrap(), second.parse().unwrap()))
     } else if Regex::new(r"copy \d+ \d+").unwrap().is_match(line) {
-        let mut split = line.split(' ');
-        split.next();
+        let mut split = line[5..].split(' ');
         let first = split.next().unwrap();
         let second = split.next().unwrap();
         Ok(Copy(first.parse().unwrap(), second.parse().unwrap()))
     } else if Regex::new(r"add \d+ \d+").unwrap().is_match(line) {
-        let mut split = line.split(' ');
-        split.next();
+        let mut split = line[4..].split(' ');
         let first = split.next().unwrap();
         let second = split.next().unwrap();
         Ok(Add(first.parse().unwrap(), second.parse().unwrap()))
     } else if Regex::new(r"sub \d+ \d+").unwrap().is_match(line) {
-        let mut split = line.split(' ');
-        split.next();
+        let mut split = line[4..].split(' ');
         let first = split.next().unwrap();
         let second = split.next().unwrap();
         Ok(Sub(first.parse().unwrap(), second.parse().unwrap()))
+    } else if Regex::new(r"mul \d+ \d+").unwrap().is_match(line) {
+        let mut split = line[4..].split(' ');
+        let first = split.next().unwrap();
+        let second = split.next().unwrap();
+        Ok(Mul(first.parse().unwrap(), second.parse().unwrap()))
+    } else if Regex::new(r"div \d+ \d+").unwrap().is_match(line) {
+        let mut split = line[4..].split(' ');
+        let first = split.next().unwrap();
+        let second = split.next().unwrap();
+        Ok(Div(first.parse().unwrap(), second.parse().unwrap()))
     } else if Regex::new(r"print \d+").unwrap().is_match(line) {
-        Ok(Print(line.split(' ').last().unwrap().parse().unwrap()))
+        Ok(Print(line[6..].parse().unwrap()))
     } else if Regex::new(r"exit \d+").unwrap().is_match(line) {
-        Ok(Exit(line.split(' ').last().unwrap().parse().unwrap()))
+        Ok(Exit(line[5..].parse().unwrap()))
     } else if line.eq("exit") {
         Ok(Exit(0))
     } else if Regex::new(r"jump addr \d+").unwrap().is_match(line) {
-        Ok(JumpAddr(line.split(' ').last().unwrap().parse().unwrap()))
+        Ok(JumpAddr(line[10..].parse().unwrap()))
     } else if Regex::new(r"_\d+").unwrap().is_match(line) {
         Ok(IntData(line[1..].parse().unwrap()))
     } else if line.len() == 1 {
